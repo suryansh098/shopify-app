07f048ecd42a2443a50d3a79f55c88d1438b0cd9
Suryansh Yadav <suryanshyadav160@gmail.com>
2021-06-06 00:24:06 +0530

    adding redux to home screen

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './Store';
 
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,27 +1,18 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React, { useEffect } from 'react';
 import Product from '../components/Product';
 import Loading from '../components/Loading';
 import ErrorMessage from '../components/ErrorMessage';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const dispatch = useDispatch();
+  const productList = useSelector( state => state.productList);
+  const {loading, error, products} = productList;
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-      const { data } = await axios.get('/api/products');
-      setLoading(false);
-      setProducts(data);
-      } catch(err) {
-        setError(err.message);
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
   return (
     <div>
       { loading ? (
