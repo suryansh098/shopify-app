95c200833d43d0ecc9b15ec19b9aef6b4a9bc068
Suryansh Yadav <suryanshyadav160@gmail.com>
2021-06-07 21:48:37 +0530

    implementing add to cart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Link
 } from 'react-router-dom';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
 function App() {
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems} = cart;
+
   return (
     <Router>
     <div className="grid-container">
       <header className="row">
         <div>
-          <a className="logo" href="/">shopify</a>
+          <Link className="logo" to="/">shopify</Link>
         </div>
         <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign in</a>
+          <Link to="/cart">Cart
+          {cartItems.length > 0 && (
+            <span className="badge">{cartItems.length}</span>
+          )}
+          </Link>
+          <Link to="/signin">Sign in</Link>
         </div>
       </header>
       <main>
--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../actions/cartActions';
 
 const CartScreen = (props) => {
-    const productId = props.match.params.id;
-    const  qty = props.location.search ? Number(props.location.search.split('=')[1]) : 1;
+  const productId = props.match.params.id;
+  const qty = props.location.search ? Number(props.location.search.split('=')[1]) : 1;
 
-    return (
-        <div>
-            <h1>Cart Screen</h1>
-            <p>Add to Cart</p>
-            <p>ProductId: {productId}</p>
-            <p>Qty: {qty}</p>
-        </div>
-    );
+  const dispatch = useDispatch()
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
+  return (
+    <div>
+      <h1>Cart Screen</h1>
+      <p>Add to Cart</p>
+      <p>ProductId: {productId}</p>
+      <p>Qty: {qty}</p>
+    </div>
+  );
 }
 
 export default CartScreen;
